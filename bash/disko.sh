916fefd7d58941ea71d3e3c66b33dee7c4ccc53c
Snekutaren <snekutaren@gmail.com>
2025-08-04 13:33:35 +0200

    Mon Aug  4 01:33:35 PM CEST 2025

--- bash/disko.sh
+++ bash/disko.sh
@@ -1,5 +1,7 @@
 #!bin/bash
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/nixrog-config/nixrog-disko-config.nix
+mkdir -p  /tmp/nixos-config
+git clone -b auto https://github.com/snekutaren/nixos-config.git /tmp/nixos-config
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/nixos-config/qemu-disko.nix
 #sudo cryptsetup luksAddKey /dev/disk/by-label/NIXOS_LUKS
 #sudo nixos-generate-config --no-filesystems --root /mnt
-sudo nixos-install --root /mnt --flake ./flake.nix -v /tmp//nixrog/flake#nixrog
+sudo nixos-install --root /mnt --flake ./flake.nix -v /tmp/nixos-config/flake#qemu
